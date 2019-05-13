ef78aa298572b1dfbb34dc6b62931a41dc0c4080
Retasu_mac <>
2019-05-13 13:19:42 +0900

    A AC

--- ABC124/A.cpp
+++ ABC124/A.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int main(void){
@@ -8,6 +9,8 @@ int main(void){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
-
+    int a,b;
+    cin >> a >> b;
+    cout << max({a+a-1,a+b,b+b-1});
     return 0;
 }
